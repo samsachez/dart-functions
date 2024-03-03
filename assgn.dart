9b780dde3a94efbef75fc0bfdb9d759694ba3d19
samsachez <samuelmusyoka003@gmail.com>
2024-03-03 17:31:56 +0300

    first commit

---
+++ assgn.dart
@@ -0,0 +1,17 @@
+//using string data type
+void main() {
+  String name=("sanchez");
+  print("my name is $name"); 
+
+// using int datatype
+  int age=30;
+  print("i am $age years old");
+  
+  //using double data type
+  double height=3.5;
+  print("my heightis $height metres");
+  //using list data type
+  List<String> cars = ["ferrari", "audi", "volkswagen"];
+  print("I own the following cars:$cars");
+  //using maps data type
+}
